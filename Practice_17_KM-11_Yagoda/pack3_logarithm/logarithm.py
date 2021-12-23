e896613715dd0ee9da5c9d29536e931ef2c3f43d
Zhenya Yagoda <91226477+Zhenya-Yagoda@users.noreply.github.com>
2021-12-23 16:58:04 +0200

    Add files via upload

---
+++ Practice_17_KM-11_Yagoda/pack3_logarithm/logarithm.py
@@ -0,0 +1,9 @@
+import math
+def log(a,b): #a=base , b=value
+    return round(math.log(b,a),5)
+
+def ln(b): 
+    return round(math.log(b,math.exp(1)),5) 
+
+def lg(b):
+    return round(math.log10(b), 5)
